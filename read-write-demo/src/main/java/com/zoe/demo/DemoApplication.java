bb96304c4a2198888797934d3bdad7a9edc00fef
gaonengli <gaonl@zoenet.cn>
2020-07-23 11:12:11 +0800

    pom 修改，去掉不必要的引入

---
+++ read-write-demo/src/main/java/com/zoe/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.zoe.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot启动类
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    /**
+     * main 方法，用户启动springboot
+     *
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
