7f80bdc33724a713e6e47d3592a26ff4b4f43b53
gaonengli <gaonl@zoenet.cn>
2020-07-31 17:27:28 +0800

    dubbo方法调用完毕，也需要弹出数据库的备库设置

--- zoe-common-rw/src/main/java/com/zoe/common/rw/dubbo/ProviderContextFilter.java
+++ zoe-common-rw/src/main/java/com/zoe/common/rw/dubbo/ProviderContextFilter.java
@@ -8,20 +8,37 @@ import org.springframework.util.StringUtils;
 
 import java.util.Map;
 
+/**
+ * @author gaonengli
+ * @date 2020/07/29
+ */
 @Activate(group = Constants.PROVIDER, order = -9999)
 public class ProviderContextFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        getAttachment(invoker, invocation);
-        return invoker.invoke(invocation);
+        boolean push = false;
+        try {
+            push = getAttachment(invoker, invocation);
+            return invoker.invoke(invocation);
+        } catch (RpcException e) {
+            throw e;
+        } finally {
+            if (push) {
+                DataSourceNameHolder.pop();
+            }
+        }
     }
 
-    private void getAttachment(Invoker<?> invoker, Invocation invocation) {
+    private boolean getAttachment(Invoker<?> invoker, Invocation invocation) {
+        boolean push = false;
         Map<String, String> attachments = RpcContext.getContext().
                 setInvoker(invoker).setInvocation(invocation).getAttachments();
         String dataSourceName = attachments.get(DataSourceNameKey.KEY);
+
         if (!StringUtils.isEmpty(dataSourceName)) {
             DataSourceNameHolder.push(dataSourceName);
+            push = true;
         }
+        return push;
     }
 }
